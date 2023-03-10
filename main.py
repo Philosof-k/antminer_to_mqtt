71070cdbc2950dab476140391a63892261a1366b
Philosof-k <77165321+Philosof-k@users.noreply.github.com>
2023-03-10 23:02:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -71,6 +71,7 @@ for x in HOSTs:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
       s.connect((x, PORT))
       s.sendall(bytes(jdata,encoding="utf-8"))
+      time.sleep(2)
       data = ''
       while 1:
           chunk = s.recv(4026)
